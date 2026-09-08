6f99d4fff4aa8bd97699339e6795acbf5944d0d8
nangosha-anne <your-email@example.com>
2026-09-08 19:18:57 +0300

    completed math_epression assignment

--- Chapter_02/math_expression.cpp
+++ Chapter_02/math_expression.cpp
@@ -1,3 +1,14 @@
 // (Compute expressions) Write a program that displays the result of;
 // (9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5)
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    double result = (9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5);
+
+    cout << "The result is: " << result << endl;
+
+    return 0;
+}
+
